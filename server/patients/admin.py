d35c1720dec5e154d0e64c3878b1b266420db9dd
Nick Reid <nickreid@nickreid.com>
2020-09-27 13:18:51 -0700

    Add admin to write/organize patient stories

--- server/patients/admin.py
+++ server/patients/admin.py
@@ -1,6 +1,20 @@
 from django.contrib import admin
 
+from admin_ordering.admin import OrderableAdmin
+
 from .models import Patient
+from .models import PatientStory
+
+class PatientStoryAdminInline(OrderableAdmin, admin.StackedInline):
+    model = PatientStory
+    ordering_field = 'order'
+    show_change_link = True
+
+    fields = [
+        'title',
+        'order',
+        'published'
+    ]
 
 @admin.register(Patient)
 class PatientAdmin(admin.ModelAdmin):
@@ -11,3 +25,27 @@ class PatientAdmin(admin.ModelAdmin):
         'name',
         'published'
     ]
+
+    inlines = [
+        PatientStoryAdminInline
+    ]
+
+@admin.register(PatientStory)
+class PatientStoryAdmin(admin.ModelAdmin):
+
+    order = ['patient_id','title','order']
+
+    list_display = [
+        'title',
+        'patient',
+        'published'
+    ]
+
+    fields = [
+        'patient',
+        'title',
+        'order',
+        'published',
+        'content'
+    ]
+

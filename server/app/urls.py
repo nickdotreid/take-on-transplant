c2113256c4cc8a582a5bf24b5a042156e5c5cf97
Nick Reid <nickreid@nickreid.com>
2020-09-27 14:06:54 -0700

    Add base templates

--- server/app/urls.py
+++ server/app/urls.py
@@ -6,10 +6,15 @@ from django.contrib.auth.views import PasswordResetDoneView
 from django.contrib.auth.views import PasswordResetConfirmView
 from django.contrib.auth.views import PasswordResetCompleteView
 
+from patients.views import PatientStoryView
+from patients.views import PatientStoryList
+
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('reset-password/<uidb64>/<token>/', PasswordResetConfirmView.as_view(), name='password_reset_confirm'),
     path('reset-password/complete/', PasswordResetConfirmView.as_view(), name='password_reset_complete'),
     path('reset-password/sent/', PasswordResetDoneView.as_view(), name='password_reset_done'),
-    path('reset-password/', PasswordResetView.as_view())
+    path('reset-password/', PasswordResetView.as_view()),
+    path('story/<patient_id>', PatientStoryView.as_view(), name='patient-story'),
+    path('', PatientStoryList.as_view(), name='patient-story-list')
 ]
--- server/patients/views.py
+++ server/patients/views.py
@@ -1,3 +1,51 @@
-from django.shortcuts import render
+from django.http import Http404
+from django.views.generic.base import TemplateView
 
-# Create your views here.
+from .models import Patient
+from .models import PatientStory
+
+class PatientStoryList(TemplateView):
+
+    template_name = "patient-story-list.html"
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+
+        patients = []
+        patients_query = Patient.objects.order_by('name') \
+        .filter(published = True)
+        for patient in patients_query.all():
+            patients.append({
+                'id': patient.id,
+                'name': patient.name
+            })
+        context['patients'] = patients
+        return context
+
+class PatientStoryView(TemplateView):
+
+    template_name = 'patient-story.html'
+
+    def get_patient(self, patient_id):
+        try:
+            return Patient.objects.get(id = patient_id)
+        except Patient.DoesNotExist:
+            raise Http404('Patient does not exist')
+    
+    def get_context_data(self, patient_id, **kwargs):
+        context = super().get_context_data(**kwargs)
+        patient = self.get_patient(patient_id)
+        context['patient'] = {
+            'id': patient.id,
+            'name': patient.name
+        }
+        stories = []
+        story_query = PatientStory.objects.filter(patient = patient)
+        for story in story_query.all():
+            stories.append({
+                'id': story.id,
+                'title': story.title,
+                'content': story.content
+            })
+        context['stories'] = stories
+        return context

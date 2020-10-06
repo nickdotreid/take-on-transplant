f3139ee1703d77504ab31eda8abf7d0539fd4640
Nick Reid <nickreid@nickreid.com>
2020-10-05 18:03:41 -0700

    Add simple popover support

--- ui/index.js
+++ ui/index.js
@@ -1,1 +1,26 @@
-console.log('hello world')
+import tippy from 'tippy.js';
+
+
+var popovers = document.querySelectorAll('a.popover');
+popovers.forEach((element) => {
+    console.log(element.children);
+    var popoverContent;
+    var i = 0;
+    while (i < element.children.length && !popoverContent) {
+        var childElement = element.children.item(i);
+        if(childElement.tagName.toLowerCase() == 'span') {
+            popoverContent = childElement.innerHTML;
+        }
+        i++
+    }
+    if(popoverContent) {
+        tippy(element, {
+            content: popoverContent,
+            allowHTML: true
+        });
+    }
+});
+
+tippy('.example',{
+    content: 'example'
+});

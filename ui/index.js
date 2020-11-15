f58245962a183599415e37e236e383bdca6660f3
Nick Reid <nickreid@nickreid.com>
2020-11-15 14:00:52 -0800

    Toss out bootstrap, start adding designed styles

--- ui/index.js
+++ ui/index.js
@@ -1,9 +1,1 @@
 
-import $ from 'jquery';
-import 'bootstrap/js/src/collapse';
-import 'bootstrap/js/src/popover';
-
-$('[data-toggle="popover"]').popover({
-    position: 'top',
-    trigger: 'click hover'
-});

a7af5e975d1f028735f5709ea5cede7370b9c026
Nick Reid <nickreid@nickreid.com>
2020-11-16 16:20:33 -0800

    Re-add tooltip to popovers

--- ui/index.js
+++ ui/index.js
@@ -1,1 +1,22 @@
 
+import tippy from 'tippy.js';
+
+function registerPopovers() {
+    document.querySelectorAll('[data-toggle="popover"]').forEach((element) => {
+        const content = element.getAttribute('data-content');
+        if (content) {
+            tippy(element, {
+                content: content
+            });
+        }
+    });
+}
+
+
+window.addEventListener('DOMContentLoaded', () => {
+
+    registerPopovers();
+
+});
+
+

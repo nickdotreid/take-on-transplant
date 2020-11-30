808c22628a80a434690fee9ed18c0a70ce086ccb
Nick Reid <nickreid@nickreid.com>
2020-11-29 19:39:07 -0800

    Add sticky elements to story page

--- ui/index.js
+++ ui/index.js
@@ -1,4 +1,4 @@
-
+import Sticky from 'sticky-js'
 import tippy from 'tippy.js';
 
 function registerPopovers() {
@@ -33,10 +33,78 @@ function registerCollapsibleContent() {
     });
 }
 
+function setupActiveLinks() {
+
+    const navLinks = document.querySelectorAll('.sidebar a');
+
+    function update() {
+        console.log('update active links');
+        navLinks.forEach((link) => {
+            let section = document.querySelector(link.hash);
+            const topPassed = section.offsetTop <= window.scrollY;
+            const bottomPassed = section.offsetTop + section.offsetHeight <= window.scrollY;
+            if (topPassed && !bottomPassed) {
+                link.classList.add('active');
+            } else {
+                link.classList.remove('active');
+            }
+        });
+    }
+
+    let timeout;
+    window.addEventListener("scroll", () => {
+        if (timeout) {
+            clearTimeout(timeout);
+        }
+        timeout = setTimeout(update, 250);
+    });
+}
+
+function setupStickyElements() {
+
+    new Sticky('.is-sticky', {
+        wrap: true,
+        stickyClass: 'is-stuck'
+    });
+
+    // const stickyElements = [];
+    // document.querySelectorAll('.is-sticky').forEach((element) => {
+    //     stickyElements.push({
+    //         'element': element,
+    //         'top': element.offsetTop,
+    //         'height': element.offsetHeight,
+    //         'width': element.offsetWidth,
+    //         'fillerElement': document.createElement('span')
+    //     });
+    // });
+    // function update() {
+    //     console.log(stickyElements);
+    //     stickyElements.forEach((_element) => {
+    //         const element = _element.element;
+    //         const fillerElement = _element.fillerElement;
+    //         const bottomPassed = _element.bottom + _element.height <= window.scrollY
+    //         if(bottomPassed) {
+    //             element.classList.add('is-stuck');
+    //             fillerElement.setAttribute('style', `disply:block; width: ${_element.width}px; height: ${_element.height}`);
+    //             element.after(fillerElement);
+    //         } else {
+    //             element.classList.remove('is-stuck');
+    //         }
+    //     })
+    // }
+
+    // let timeout;
+    // window.addEventListener("scroll", () => {
+    //     if (timeout) {
+    //         clearTimeout(timeout);
+    //     }
+    //     timeout = setTimeout(update, 150);
+    // });
+}
+
 window.addEventListener('DOMContentLoaded', () => {
     registerCollapsibleContent();
     registerPopovers();
-
+    setupActiveLinks();
+    setupStickyElements();
 });
-
-

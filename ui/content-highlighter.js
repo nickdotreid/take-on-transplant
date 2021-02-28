475518141f37b007e53792010ca8df56e9226dca
Nick Reid <nickreid@nickreid.com>
2021-02-28 09:06:51 -0800

    Refactored to class

--- ui/content-highlighter.js
+++ ui/content-highlighter.js
@@ -1,107 +1,124 @@
 
-function save_highlight_content(highlightId) {
-    if (!document.querySelector("input[name='csrfmiddlewaretoken']")) return;
-    var csrf_token = document.querySelector("input[name='csrfmiddlewaretoken']").value;
-    console.log("Now save the content object that the highlight is a part of...", highlightId);
-
-    var highlight = document.querySelector(`[highlight-id="${highlightId}"]`);
-    if (!highlight) return;
-    var content_container = highlight.closest('[content-id]');
-    
-    var request = new XMLHttpRequest();
-    request.responseType = 'json';
-    request.open("POST", `/highlights/${highlightId}`);
-    request.setRequestHeader('content-type', 'application/json');
-    request.setRequestHeader('X-CSRFToken', csrf_token);
-    request.send(JSON.stringify({
-        'text': highlight.innerText,
-        'contentId': content_container.getAttribute('content-id'),
-        'content': content_container.innerHTML
-    }));
+function debounce(fn) {
+    var timeout;
+    return function() {
+        if(timeout) clearTimeout(timeout);
+        timeout = setTimeout(fn, 150);
+    }
 }
 
-function remove_highlight(highlightId) {
-    document.querySelectorAll(`[highlight-id="${highlightId}"]`).forEach(function(element){
-        element.childNodes.forEach(function(childElement) {
-            element.before(childElement);
+export class ContentHighlighter {
+
+    #CSRF_TOKEN;
+
+    constructor(csrfToken) {
+        this.#CSRF_TOKEN = csrfToken;
+
+
+        document.addEventListener('selectionchange', debounce(() => {
+            this.selectionChange()
+        }));
+    }
+
+    selectionChange() {
+        if(this.highlightButton) this.highlightButton.remove();
+        
+        var selection = document.getSelection();
+        var range = selection.getRangeAt(selection.rangeCount-1);    
+        if (range.collapsed) return;
+        
+        if (!this.getNodeContentId(range.commonAncestorContainer)) return;
+
+        this.highlightButton = this.createHighlightButton(range);
+        this.highlightButton.addEventListener('click', () => {
+            selection.collapseToEnd();
+            this.createHighlight(range);
         });
-        element.remove();
-    });
-}
+    }
 
-function highlight_text_as(highlightId, range) {
-    var highlight = document.createElement('span');
-    highlight.classList.add('highlight');
-    highlight.setAttribute('highlight-id',highlightId);
-    highlight.addEventListener("click", function(event) {
-        event.preventDefault();
-        remove_highlight(highlightId);
-    });
-    range.surroundContents(highlight);
-}
-
-function make_highlight(range) {
-    if (!document.querySelector("input[name='csrfmiddlewaretoken']")) return;
-
-    var request = new XMLHttpRequest()
-    request.responseType = 'json';
-    var csrf_token = document.querySelector("input[name='csrfmiddlewaretoken']").value;
-    request.addEventListener("loadend", function(event) {
-        console.log(request.responseType, request.response);
-        if(request.response) {
-            var highlightId = request.response['id'];
-            highlight_text_as(highlightId, range);
-            save_highlight_content(highlightId);
+    getNodeContentId(node) {
+        if(node.nodeName == '#text') {
+            node = node.parentElement;
         }
-    })
-    request.open("POST", "/highlights");
-    request.setRequestHeader('Content-type', 'application/json');
-    request.setRequestHeader('X-CSRFToken', csrf_token);
-    request.send();
-}
-
-
-var selectionChangeDebounced;
-function debounceSelectionChange() {
-    if (selectionChangeDebounced) {
-        clearTimeout(selectionChangeDebounced);
-    }
-    selectionChangeDebounced = setTimeout(selectionChange, 1000);
-}
-
-var selectionEnabled = false;
-function selectionChange() {
-    var selection = document.getSelection();
-    console.log(selection.rangeCount);
-    var range = selection.getRangeAt(selection.rangeCount-1);
-
-    if (range.collapsed || !selectionEnabled) {
-        return;
-    }
-
-    var highlightButton = document.createElement('button')
-    highlightButton.innerText = 'Make a highlight'
-    var boundingCoords = range.getBoundingClientRect();
-    highlightButton.style = 'position:fixed;top:'+boundingCoords.top+'px;left:'+boundingCoords.right+'px;';
-    var body = document.querySelector('body');
-    body.appendChild(highlightButton);
-    highlightButton.addEventListener('click', function() {
-        highlightButton.remove();
-        selection.collapseToEnd();
-        make_highlight(range);
-    });
-}
-
-export function setupHighligher() {
-    document.addEventListener('selectionchange', debounceSelectionChange);
-    document.addEventListener('keydown', function(event) {
-        if (event.code.includes('Shift')) {
-            selectionEnabled = true;
+        var content_container = this.getHighlightContentNode(node);
+        if (content_container) {
+            return content_container.getAttribute('content-id');
         } else {
-            selectionEnabled = false;
+            return false;
         }
-    });
-    document.addEventListener('keyup', function(event) {
-        selectionEnabled = false;
-    });
+    }
+
+    getHighlightContentNode(highlightNode) {
+        var content_container = highlightNode.closest('[content-id]');
+        if (content_container) {
+            return content_container;
+        } else {
+            return false;
+        }
+    }
+
+    createHighlightButton(range) {
+        var highlightButton = document.createElement('button');
+        highlightButton.innerText = 'Make a highlight';
+        var boundingCoords = range.getBoundingClientRect();
+        highlightButton.style = 'position:fixed;top:'+boundingCoords.top+'px;left:'+boundingCoords.right+'px;';
+        var body = document.querySelector('body');
+        body.appendChild(highlightButton);
+        return highlightButton
+    }
+
+    createHighlight(range) {
+        var request = new XMLHttpRequest()
+        request.responseType = 'json';
+        request.addEventListener("loadend", () => {
+            if(request.response) {
+                var highlightId = request.response['id'];
+                this.highlightRangeAs(range, highlightId);
+                this.updateHighlightContent(highlightId);
+            }
+        })
+        request.open("POST", "/highlights");
+        request.setRequestHeader('Content-type', 'application/json');
+        request.setRequestHeader('X-CSRFToken', this.#CSRF_TOKEN);
+        request.send();
+    }
+
+    updateHighlightContent(highlightId) {
+        var highlight = document.querySelector(`[highlight-id="${highlightId}"]`);
+        var content_container = this.getHighlightContentNode(highlight);
+        
+        if (!highlight || !content_container) return;
+
+        var request = new XMLHttpRequest();
+        request.responseType = 'json';
+        request.open("POST", `/highlights/${highlightId}`);
+        request.setRequestHeader('content-type', 'application/json');
+        request.setRequestHeader('X-CSRFToken', this.#CSRF_TOKEN);
+        request.send(JSON.stringify({
+            'text': highlight.innerText,
+            'contentId': content_container.getAttribute('content-id'),
+            'content': content_container.innerHTML
+        }));
+    }
+
+    remove_highlight(highlightId) {
+        document.querySelectorAll(`[highlight-id="${highlightId}"]`).forEach(function(element){
+            element.childNodes.forEach(function(childElement) {
+                element.before(childElement);
+            });
+            element.remove();
+        });
+    }
+
+    highlightRangeAs(range, highlightId) {
+        var highlight = document.createElement('span');
+        highlight.classList.add('highlight');
+        highlight.setAttribute('highlight-id',highlightId);
+        highlight.addEventListener("click", function(event) {
+            event.preventDefault();
+            remove_highlight(highlightId);
+        });
+        range.surroundContents(highlight);
+    }
+
 }
--- ui/index.js
+++ ui/index.js
@@ -1,7 +1,6 @@
-import Sticky from 'sticky-js'
 import tippy, {followCursor, hideAll} from 'tippy.js';
 import Sortable from 'sortablejs';
-import {setupHighligher} from './content-highlighter.js'
+import {ContentHighlighter} from './content-highlighter.js'
 
 function registerPopovers() {
     document.querySelectorAll('[data-toggle="popover"]').forEach((element) => {
@@ -55,27 +54,20 @@ function setupActiveLinks() {
     });
 }
 
-function setupStickyElements() {
-
-    new Sticky('.is-sticky', {
-        wrap: true,
-        stickyClass: 'is-stuck'
-    });
-
-}
-
 function makeSortableLists() {
-    console.log('hi')
     document.querySelectorAll('.sortable').forEach(function(element) {
         new Sortable(element);
-        console.log('sort me!');
-    })
+    });
 }
 
 window.addEventListener('DOMContentLoaded', () => {
     registerPopovers();
-    // setupStickyElements();
     setupActiveLinks();
-    setupHighligher();
     makeSortableLists();
+
+    if(document.querySelector("input[name='csrfmiddlewaretoken']")) {
+        var csrfToken = document.querySelector("input[name='csrfmiddlewaretoken']").value;
+        new ContentHighlighter(csrfToken);
+    }
+
 });
